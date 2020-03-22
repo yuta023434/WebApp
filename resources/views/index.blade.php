56dfa5bb2ef93d7b8a13bffd920b6e4e27666dc9
username <zityouponoka023434@gmail.com>
2020-03-22 13:43:55 +0900

    add secound commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,12 @@
   {{-- 検索条件テーブル --}}
   <h3 style="border-bottom: 1px solid #000;border-left: 10px solid #000;padding: 7px;margin-top:10px;">商品情報一覧</h3>
   <div id="contents_search">
+    <span>GitTest</span>
     <form>
       <table class="table table_border_radius">
         <thead>
           <tr>
-            <th colspan="6">検索条件git</th>
+            <th colspan="6">検索条件</th>
           </tr>
         </thead>
         <tbody>
