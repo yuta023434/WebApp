89ee43bcda07cd77d5c17ead9fc61f6ace20bb79
username <zityouponoka023434@gmail.com>
2020-05-08 08:10:36 +0900

    a

--- database/migrations/2020_05_06_214528_create_t_goods_table.php
+++ database/migrations/2020_05_06_214528_create_t_goods_table.php
@@ -13,25 +13,25 @@ class CreateTGoodsTable extends Migration
      */
     public function up()
     {
-        if (Schema::hasTable('t_goods'))
-        {
-            return;
-        }
+        // if (Schema::hasTable('t_goods'))
+        // {
+        //     return;
+        // }
 
         Schema::create('t_goods', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('un_id',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('ユニークID');
-            $table->string('goods_number',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('商品番号');
-            $table->string('goods_name',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('商品名');
-            $table->integer('goods_price',255)->default(NULL)->comment('商品価格');
-            $table->integer('goods_stock',255)->default(NULL)->comment('商品個数');
-            $table->text('intro_txt')->collation('utf8mb4_unicode_ci')->comment('紹介文');
-            $table->timestamp('up_date')->default(NULL)->comment('更新日時');
-            $table->timestamp('ins_date')->default(NULL)->comment('追加日時');
-            $table->tinyInteger('disp_flg')->unsigned()->comment('表示フラグ');
-            $table->tinyInteger('delete_flg')->unsigned()->default(0)->comment('削除フラグ');
-            $table->engine = 'InnoDB';
-            $table->collation = 'utf8mb4_unicode_ci';
+            // $table->string('un_id',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('ユニークID');
+            // $table->string('goods_number',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('商品番号');
+            // $table->string('goods_name',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('商品名');
+            // $table->integer('goods_price',255)->default(NULL)->comment('商品価格');
+            // $table->integer('goods_stock',255)->default(NULL)->comment('商品個数');
+            // $table->text('intro_txt')->collation('utf8mb4_unicode_ci')->comment('紹介文');
+            // $table->timestamp('up_date')->default(NULL)->comment('更新日時');
+            // $table->timestamp('ins_date')->default(NULL)->comment('追加日時');
+            // $table->tinyInteger('disp_flg')->unsigned()->comment('表示フラグ');
+            // $table->tinyInteger('delete_flg')->unsigned()->default(0)->comment('削除フラグ');
+            // $table->engine = 'InnoDB';
+            // $table->collation = 'utf8mb4_unicode_ci';
         });
     }
 
@@ -42,6 +42,6 @@ class CreateTGoodsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('t_goods');
+        //Schema::dropIfExists('t_goods');
     }
 }
