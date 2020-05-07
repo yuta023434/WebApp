9a9a69c2090740ed3a04682bcb282764822e18cd
username <zityouponoka023434@gmail.com>
2020-05-08 08:26:39 +0900

    a

--- database/migrations/2020_05_06_214528_create_t_goods_table.php
+++ database/migrations/2020_05_06_214528_create_t_goods_table.php
@@ -18,8 +18,9 @@ class CreateTGoodsTable extends Migration
         //     return;
         // }
 
-        Schema::create('t_goods', function (Blueprint $table) {
-            $table->increments('id');
+        //テーブル作成
+        // Schema::create('t_goods', function (Blueprint $table) {
+        //     $table->increments('id');
             // $table->string('un_id',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('ユニークID');
             // $table->string('goods_number',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('商品番号');
             // $table->string('goods_name',255)->default(NULL)->collation('utf8mb4_unicode_ci')->comment('商品名');
@@ -28,10 +29,15 @@ class CreateTGoodsTable extends Migration
             // $table->text('intro_txt')->collation('utf8mb4_unicode_ci')->comment('紹介文');
             // $table->timestamp('up_date')->default(NULL)->comment('更新日時');
             // $table->timestamp('ins_date')->default(NULL)->comment('追加日時');
-            // $table->tinyInteger('disp_flg')->unsigned()->comment('表示フラグ');
-            // $table->tinyInteger('delete_flg')->unsigned()->default(0)->comment('削除フラグ');
+            // $table->tinyInteger('disp_flg',1)->comment('表示フラグ');
+            // $table->tinyInteger('delete_flg,1)->default(0)->comment('削除フラグ');
             // $table->engine = 'InnoDB';
             // $table->collation = 'utf8mb4_unicode_ci';
+        // });
+
+        //テーブル更新
+        Schema::table('t_goods', function(Blueprint $table){
+            $table->string('un_id',255)->after('id')->default(NULL)->collation('utf8mb4_unicode_ci')->comment('ユニークID');
         });
     }
 
