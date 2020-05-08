b32b6b15e514692551a12468ae46750bade978df
username <zityouponoka023434@gmail.com>
2020-05-08 16:25:37 +0900

    fix after of migration files

--- database/migrations/2020_05_08_145341_add_column_goods_price_to_t_goods_table.php
+++ database/migrations/2020_05_08_145341_add_column_goods_price_to_t_goods_table.php
@@ -14,7 +14,7 @@ class AddColumnGoodsPriceToTGoodsTable extends Migration
     public function up()
     {
         Schema::table('t_goods', function (Blueprint $table) {
-            $table->integer('goods_price',255)->default(0)->after('goods_name')->comment('商品価格');
+            $table->integer('goods_price',255)->after('goods_name')->comment('商品価格');
         });
     }
 
