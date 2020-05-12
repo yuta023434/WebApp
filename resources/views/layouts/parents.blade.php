0bcc54e98c5bb72daf6a6a79269d11d1894934bd
username <zityouponoka023434@gmail.com>
2020-05-12 09:35:03 +0900

    aa

--- resources/views/layouts/parents.blade.php
+++ resources/views/layouts/parents.blade.php
@@ -8,7 +8,7 @@
   <title>@yield('title')</title>
 
   {{-- BootstrapベースCSSファイル --}}
-  <link href="{{asset('../vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   {{-- ページレイアウト関連テンプレートCSSファイル --}}
   <link href="{{asset('css/modern-business.css')}}" rel="stylesheet">
